a42b22f7f4a80594aeae79786de87583d637568c
ozgeny <ozgenyazicii@gmail.com>
2019-07-03 22:08:40 +0300

    First

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Düşmanların belirli hareketleri olacak.
+ * Uyuyan veya bir işle meşgul olan düşmanlar belli yükseklikteki sese tepki verebilecekler.
+ * Bunların dışında düşmanlar herhangi bir ses duymasalar bile bulundukları yerden kalkabilecekler.Örneğin tuvalete gitmek ya da gece uyanıp televizyon izlemek gibi.
+ * Bu düşmanlar televizyon başına geçtiği zaman uyuklayabilecekler.
+ * Karakterimizin evde saklanabileceği yerler olacak.Çünkü ışıklar açıldığı zaman düşmanların görüş açısı büyüyecek.
+
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+
+{
+
+    public static PlayerController instance;
+
+    public LayerMask whatCanBeClickedOn;
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray,out hitInfo, 100, whatCanBeClickedOn))
+            {
+
+                navMeshAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+   
+}
